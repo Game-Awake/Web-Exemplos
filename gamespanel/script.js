dce3a831d9f345e198e8575d7152614fad6cb274
Bruno Duarte <bruno.duartec@gmail.com>
2021-04-02 19:09:46 -0300

    add game panel

--- gamespanel/script.js
+++ gamespanel/script.js
@@ -1,4 +1,4 @@
-let scores = [
+let gamesInfo = [
   {
     name: "Game 1",
     description: "Jogo exemplo 1",
@@ -14,6 +14,12 @@ let scores = [
   {
     name: "Game 1",
     description: "Jogo exemplo 1",
+    image: "snake",
+    link: "http://google.com.br",
+  },
+  {
+    name: "Game 2",
+    description: "Jogo exemplo 2",
     image: "snake",
     link: "http://google.com.br",
   },
@@ -25,8 +31,6 @@ let scores = [
   },
 ];
 
-var rows = 2;
-
 // Get the elements with class="column"
 var elements = document.getElementsByClassName("column");
 
@@ -35,17 +39,26 @@ var i;
 
 function createGamePanel(item) {
   let panel = document.createElement("div");
+  panel.classList.add("column");
 
   let name = document.createElement("div");
   name.classList.add("name");
+  name.innerText = item.name;
 
   let description = document.createElement("div");
   description.classList.add("description");
+  description.innerText = item.description;
 
   let imageDiv = document.createElement("div");
   imageDiv.classList.add("img");
 
   let image = document.createElement("img");
+  image.src = `assets/${item.image}.png`;
+  imageDiv.appendChild(image);
+
+  panel.appendChild(name);
+  panel.appendChild(description);
+  panel.appendChild(imageDiv);
 
   return panel;
 }
@@ -63,6 +76,33 @@ function gridView() {
     elements[i].style.width = "50%";
   }
 }
+
+function createGameBoard() {
+  var maxRows = 2;
+  let length = gamesInfo.length;
+  let rows = Math.round(length / maxRows);
+
+  let gameBoardDiv = document.getElementById("gameBoard");
+
+  console.log("A", gameBoardDiv);
+  let itensPlaced = 0;
+
+  for (let columns = 0; columns < rows; columns++) {
+    let row = document.createElement("div");
+    row.classList.add("row");
+
+    for (let rows = 0; rows < maxRows; rows++) {
+      if (itensPlaced < length) {
+        const element = gamesInfo[itensPlaced++];
+        let column = createGamePanel(element);
+        row.appendChild(column);
+      }
+    }
+    gameBoardDiv.appendChild(row);
+  }
+}
+
+createGameBoard();
 
 /* Optional: Add active class to the current button (highlight it) */
 var container = document.getElementById("btnContainer");

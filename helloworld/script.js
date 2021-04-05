dd97d075415eaed829b5f594980d9b2c6420c7fa
Bruno Duarte <bruno.duartec@gmail.com>
2021-04-05 17:39:53 -0300

    Add variaveis

---
+++ helloworld/script.js
@@ -0,0 +1,27 @@
+let pao = 1;
+let leite = 2;
+let abacate = 1;
+
+console.log(pao);
+console.log(leite);
+console.log(abacate);
+
+let itemsdacompra = pao + leite + abacate;
+
+console.log(itemsdacompra);
+
+let teste = true; // booleano
+
+console.log(teste);
+
+let listadecompras = "Lista bonita"; //string
+
+console.log(listadecompras);
+
+//DESAFIO
+
+//declarar as variaveis com preço de cada coisa
+
+// calcular quanto custa essa compra;
+
+//fazer a conta do preço total
